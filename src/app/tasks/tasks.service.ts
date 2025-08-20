cbcb4fd4a875c84560a2453fbc312f886835ab08
liam-chadwick <liam_2112@hotmail.co.uk>
2025-08-20 15:20:45 +0100

    added a logging service and injected services into services

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -1,11 +1,14 @@
-import { Injectable, signal } from "@angular/core";
+import { inject, Injectable, signal } from "@angular/core";
+
 import { Task, TaskStatus } from "./task.model";
+import { LoggingService } from "../logging.service";
 
 @Injectable({
     providedIn: 'root'
 })
 export class TasksService {
     private tasks = signal<Task[]>([]);
+    private loggingService = inject(LoggingService);
 
     allTasks = this.tasks.asReadonly();
 
@@ -16,9 +19,12 @@ export class TasksService {
             status: 'OPEN'
         };
         this.tasks.update((oldTasks) => [...oldTasks, newTask]);
+        this.loggingService.log('ADDED TASK WITH TITLE' + taskData.title);
     }
 
     updateTaskStatus(taskId: string, newStatus: TaskStatus) {
-        this.tasks.update((oldTasks) => oldTasks.map((task) => task.id === taskId ? {...task, status: newStatus} : task))
+        this.tasks.update((oldTasks) => oldTasks.map((task) => task.id === taskId ? {...task, status: newStatus} : task));
+        this.loggingService.log('CHANGE TASK STATUS TO' + newStatus);
+
     }
 }
